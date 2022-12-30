6d275b3c930ad081df7f2048d68b7bf36962b141
wndskim <wndskim01@gmail.com>
2022-12-30 18:56:00 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -40,10 +40,10 @@ if job=='코스피200':
         st.write(i, ticker, stock.get_market_ticker_name(ticker))
 
 # url = 'https://gitlab.com/wndskim/repo/-/raw/master/data.xlsx'
-url = 'https://gitlab.com/wndskim/상한가_300억이상_거래 종목.xlsx'
+url = 'https://gitlab.com/wndskim/stock/main/상한가_300억이상_거래 종목.xlsx'
 
-data = pd.read_excel('/wndskim/stock/main/상한가_300억이상_거래 종목.xlsx','Sorted')
-# data = pd.read_excel(url)
+# data = pd.read_excel('/wndskim/stock/main/상한가_300억이상_거래 종목.xlsx','Sorted')
+data = pd.read_excel(url)
 
 st.dataframe(data)
 
