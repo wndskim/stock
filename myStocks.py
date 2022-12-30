b02bdce5185117fa652d5b0fae3b6745ebda8c0b
wndskim <wndskim01@gmail.com>
2022-12-30 17:52:35 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -14,11 +14,14 @@ import matplotlib as plt
 
 
 # Side Bar 생성
-job=st.sidebar.selectbox('선택',['코스피2000','인덱스 종류'])
+job=st.sidebar.selectbox('선택',['선택','코스피200','인덱스 종류'])
 if job=='코스피200': pass
 if job=='인덱스 종류':
     for ticker in stock.get_index_ticker_list():
         st.write(ticker, stock.get_index_ticker_name(ticker))
+if job=='코스피200':
+    pdf = stock.get_index_portfolio_deposit_file("1028")
+    st.write(pdf)
 
 
 
