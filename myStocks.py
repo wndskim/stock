fd1d32f318895d7612be8466485a80084500f8f5
wndskim <wndskim01@gmail.com>
2022-12-30 17:28:57 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -6,12 +6,11 @@ import numpy as np
 from pykrx import stock
 import matplotlib as plt
 
-st.subheader('민정희 이것  잘 보여?????')
-st.title('This is title')
-st.header('This is header')
-st.subheader('This is subheader')
-st.text('This is text')
-st.caption('This is caption')
+# st.title('This is title')
+# st.header('This is header')
+# st.subheader('This is subheader')
+# st.text('This is text')
+# st.caption('This is caption')
 
 st.write('2022년12월1일부터 2022년12월29일까지 전종목 가격 변동 - 하락률 순')
 df = stock.get_market_price_change("20221201", "20221231", market='ALL')
