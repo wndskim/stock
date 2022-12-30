5ea7c28ed0651d2128f24633ad451bb5e892465b
wndskim <72592534+wndskim@users.noreply.github.com>
2022-12-30 16:49:13 +0900

    Add files via upload

--- myStocks.py
+++ myStocks.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from pykrx import stock
 
 st.title('This is title')
 st.header('This is header')
@@ -10,10 +11,7 @@ st.subheader('This is subheader')
 st.text('This is text')
 st.caption('This is caption')
 
-st.title('민정희 공부 잘하고 있어?')
-st.title('민정희 공부 잘하고 있어?')
-st.title('민정희 공부 잘하고 있어?')
-st.title('민정희 공부 잘하고 있어?')
-st.title('민정희 공부 잘하고 있어?')
-st.title('민정희 공부 잘하고 있어?')
+df = stock.get_market_price_change("20220901", "20221130", market='ALL')
+
+st.dataframe(df)
 
