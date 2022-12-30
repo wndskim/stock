5be565f57c72e5a20ca5c7b553eba1c5cebfba8d
wndskim <72592534+wndskim@users.noreply.github.com>
2022-12-30 17:06:05 +0900

    Add files via upload

--- myStocks.py
+++ myStocks.py
@@ -13,7 +13,7 @@ st.subheader('This is subheader')
 st.text('This is text')
 st.caption('This is caption')
 
-st.subheader('2022년12월1일부터 2022년12월29일까지 전종목 가격 변동 - 하락률 순')
+st.write('2022년12월1일부터 2022년12월29일까지 전종목 가격 변동 - 하락률 순')
 df = stock.get_market_price_change("20221201", "20221231", market='ALL')
 df.sort_values(by='등락률', ascending=True, inplace=True)
 
