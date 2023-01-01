c503d576e8c0725d8d7bb87e2967079f032ae8d1
wndskim <wndskim01@gmail.com>
2023-01-01 16:33:11 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from pykrx import stock
 import matplotlib as plt
+# import yfinance as yf
 
 # import gspread
 # from oauth2client.service_account import ServiceAccountCredentials
@@ -27,7 +28,7 @@ import matplotlib as plt
 
 
 # Side Bar 생성
-job=st.sidebar.selectbox('선택',['선택','코스피200','인덱스 종류', '특징주 보기'])
+job=st.sidebar.selectbox('선택',['선택','종목별 OHLCV', '코스피200','인덱스 종류', '특징주 보기'])
 if job=='선택': pass
 
 if job=='인덱스 종류':
@@ -39,24 +40,30 @@ if job=='코스피200':
     for i, ticker in enumerate(tickers):
         st.write(i, ticker, stock.get_market_ticker_name(ticker))
 
+
+
+# # Get the data for the stock Apple by specifying the stock ticker, start date, and end date
+# data = yf.download('005930.KS', start='2020-01-01', end='2022-12-31')
+
+# # Print the data
+# print(data)
+
+
 # url = 'https://gitlab.com/wndskim/repo/-/raw/master/data.xlsx'
 # url = 'https://gitlab.com/wndskim/stock/main/상한가_300억이상_거래 종목.xlsx'
 # url='https://github.com/wndskim/stock/blob/b9b1d3205b052c18a7a67c4c3e8b88948238b522/data.xlsx'
 # data = pd.read_excel('/wndskim/stock/main/상한가_300억이상_거래 종목.xlsx','Sorted')
 
-url = 'https://github.com/wndskim/stock/blob/main/data.xlsx'
-data = pd.read_excel(url,'Sorted')
+# url = 'https://github.com/wndskim/stock/blob/main/data.xlsx'
+# data = pd.read_excel(url,'Sorted')
 
-st.dataframe(data)
-
-
-import pandas as pd
+# st.dataframe(data)
 
 # Read the Excel file from GitHub
-df = pd.read_excel('https://github.com/YourGitHubName/YourRepositoryName/YourExcelFileName.xlsx')
+# df = pd.read_excel('https://github.com/YourGitHubName/YourRepositoryName/YourExcelFileName.xlsx')
 
 # Print the contents of the Excel file
-print(df)
+# print(df)
 
 
 
