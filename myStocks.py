f8ff218fcef03f5b8aceec9ae2d2b45aea754faf
wndskim <wndskim01@gmail.com>
2023-01-02 17:15:12 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -66,8 +66,8 @@ if job=='가격 변동률':
 ##############################################################
 def main():
     # st.set_page_config(page_title='My Stock Management',
-    st.set_page_config(layout='wide'
-                    )
+    # st.set_page_config(layout='wide'
+    #                 )
 
     version=st.__version__
     st.markdown(f'''
