f9684540bf66ca05bd6873aa7b5c6204fff56c28
wndskim <wndskim01@gmail.com>
2023-01-02 19:00:37 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -8,6 +8,7 @@ import matplotlib as plt
 import yfinance as yf
 import openpyxl
 import OpenDartReader
+from datetime import datetime, timedelta
 
 API_KEY="d538a1a0a4263cb8fbfa06a7429937ea86fc1aa1"
 dart=OpenDartReader(API_KEY)
@@ -48,7 +49,7 @@ def main():
                 ''')
 
     # 금일 금강원 공시 내역
-    today=datetime.datetime.now().strftime('%Y%m%d')
+    today=datetime.now().strftime('%Y%m%d')
     df=dart.list(start=today, end=today, final=False)
     st.markdown('-----')
     st.text('금일 금감원 공시 건수:'+str(len(df))+'건')
