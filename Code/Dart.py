628a7a476c7bf7d6ded85a6492f4a0b3dbd71d2f
wndskim <wndskim01@gmail.com>
2023-01-03 13:12:26 +0900

    Update Dart.py

--- Code/Dart.py
+++ Code/Dart.py
@@ -10,6 +10,15 @@ def 금감원_공시내역_보기():
     # 금일 금강원 공시 내역
     today=datetime.now().strftime('%Y%m%d')
     df=dart.list(start=today, end=today, final=False)
+
+    df['날짜']=df['rcept_dt']
+    df['티커']=df['stock_code']
+    df['종목명']=df['corp_name']
+    df['공시내용']=df['report_nm']
+
+    df=df.drop(['corp_code','corp_name','stock_code','corp_cls','report_nm','rcept_no','rcept_dt'], axis=1)
+
+
     st.markdown('-----')
     st.text('금일 금감원 공시 건수:'+str(len(df))+'건')
     st.dataframe(df)
