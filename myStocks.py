aade50960ff5cabd74af8ff2a27d6ae602bab715
wndskim <wndskim01@gmail.com>
2023-01-03 18:37:39 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -57,7 +57,7 @@ def main():
     # st.write('[경기상황정리](https://docs.google.com/spreadsheets/d/14OhuYvmkb3dZUIpxP9mu9uS1zNxUY3gFnafHOWOYs5o/edit#gid=719655173)')
     # st.write('[KT](https://docs.google.com/spreadsheets/d/1A_8rYBwU35sfWJezUcKGaFiofMc0cp39TZQCkdSA6Rw/edit#gid=0)')
     if job=='관심주 및 보유주':
-        df=pd.read_excel('./Data/관심종목.xlsx')
+        df=pd.read_excel('./Data/관심종목.xlsx', index=False)
         st.write('[경기상황정리](https://docs.google.com/spreadsheets/d/14OhuYvmkb3dZUIpxP9mu9uS1zNxUY3gFnafHOWOYs5o/edit#gid=719655173)')
         st.write('[기법정리](https://docs.google.com/spreadsheets/d/1tJg4kfIIpt17LNKXoKwzzallnXPmyCzMF1DhIIw1Q-8/edit#gid=1186881965)')
         st.write('[KT(030200) 보유주](https://docs.google.com/spreadsheets/d/1A_8rYBwU35sfWJezUcKGaFiofMc0cp39TZQCkdSA6Rw/edit#gid=0)')
