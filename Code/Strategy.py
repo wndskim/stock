ac15ddb5e11e2850bedf804d37c0070729dc294d
wndskim <wndskim01@gmail.com>
2023-01-07 17:44:26 +0900

    Update Strategy.py

--- Code/Strategy.py
+++ Code/Strategy.py
@@ -2,12 +2,9 @@ import streamlit as st
 
 def Strategy_2023_01(pbr):
 
-    try:
-        st.markdown(f'최근 코스피 PBR은 :blue[{pbr}]로 매우 저평가 되어있다. :orange[이번 사이클의 상승 여력이 있는 종목이나] \
-            :violet[지금 바닥에 있는 종목중 다음 사이클에 상승 예상 종목을 발굴하여 진입할 준비가 필요하다].')
-    except: pass
+    st.markdown(f'최근 코스피 PBR은 :blue[{pbr}]로 매우 저평가 되어있다. :orange[이번 사이클의 상승 여력이 있는 종목이나] \
+        :violet[지금 바닥에 있는 종목중 다음 사이클에 상승 예상 종목을 발굴하여 진입할 준비가 필요하다].')
 
-    st.text('')
     st.text('')
     st.markdown('**2023년 1월 현재 시장은 하향추세로 다음 사항을 매일 한번 점검한다.**')
     st.text('')
@@ -16,6 +13,10 @@ def Strategy_2023_01(pbr):
     st.markdown('**:red[2. 코스피 지수가 볼린저밴드(40,2)의 하한선을 하향 돌파 하였는가?]**')
     st.markdown('**:violet[3. 코스피 지수의 RSI(10)이 30 이하인가?]**')
 
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
     st.markdown("Text can be :blue[azul], but also :orange[laranja]. And of course it can be \
     :red[red]. And :green[verde]. And look at this :violet[violeta]!")
 
