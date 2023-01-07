bbafa950c62ecf8a08dd9e37bdb78136086a1742
wndskim <wndskim01@gmail.com>
2023-01-07 17:37:13 +0900

    Update Strategy.py

--- Code/Strategy.py
+++ Code/Strategy.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
-def Strategy_2023_01():
+def Strategy_2023_01(pbr):
 
     st.markdown('**2023년 1월 현재 시장은 하향추세로 다음 사항을 매일 한번 점검한다.**')
+
+
+    st.markdown(f'최근 코스피 PBR은 :blue[{pbr}]로 매우 저평가 되어있다. :orange[이번 사이클의 상승 여력이 있는 종목이나] \
+        :violet[지금 바닥에 있는 종목중 다음 사이클에 상승 예상 종목을 발굴하여 진입할 준비가 필요하다].')
+
     st.markdown('**:blue[1. 코스피의 현재 PBR이 0.90이하인가?]**')
     st.markdown('**:red[2. 코스피 지수가 볼린저밴드(40,2)의 하한선을 하향 돌파 하였는가?]**')
     st.markdown('**:violet[3. 코스피 지수의 RSI(10)이 30 이하인가?]**')
