1d53ba00258b465821a2f79d52caed92e3eeb493
wndskim <wndskim01@gmail.com>
2023-01-07 15:38:51 +0900

    Update Dart.py

--- Code/Dart.py
+++ Code/Dart.py
@@ -34,13 +34,11 @@ def Index_Fundamental_조회(시작일, 종료일, 마켓):
     df=stock.get_index_fundamental(시작일, 종료일, market)
     df.drop(['선행PER'], inplace=True, axis=1)
     df.reset_index(inplace=True)
+    df.sort_values(by='날짜',ascending=False,inplace=True)
     df['날짜']=df['날짜'].dt.strftime('%Y-%m-%d')
     df['종가'] = df['종가'].map('{:,.2f}'.format)
     df['등락률'] = df['등락률'].map('{:,.2f}'.format)
     df['PER'] = df['PER'].map('{:,.2f}'.format)
     df['PBR'] = df['PBR'].map('{:,.2f}'.format)
     df['배당수익률'] = df['배당수익률'].map('{:,.2f}'.format)
-
-
-
     return df
