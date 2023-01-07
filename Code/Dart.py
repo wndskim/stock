027eff2518e634674aba76bf96b0cb8b1196a855
wndskim <wndskim01@gmail.com>
2023-01-07 15:36:40 +0900

    Update Dart.py

--- Code/Dart.py
+++ Code/Dart.py
@@ -32,16 +32,15 @@ def Index_Fundamental_조회(시작일, 종료일, 마켓):
     if 마켓=='코스피': market='1001'
     else: market='2001' # 코스닥
     df=stock.get_index_fundamental(시작일, 종료일, market)
+    df.drop(['선행PER'], inplace=True, axis=1)
     df.reset_index(inplace=True)
     df['날짜']=df['날짜'].dt.strftime('%Y-%m-%d')
     df['종가'] = df['종가'].map('{:,.2f}'.format)
-    # df['종가']=df['종가'].round(2)
-    # df['PBR']=df['PBR'].round(2)
+    df['등락률'] = df['등락률'].map('{:,.2f}'.format)
+    df['PER'] = df['PER'].map('{:,.2f}'.format)
+    df['PBR'] = df['PBR'].map('{:,.2f}'.format)
+    df['배당수익률'] = df['배당수익률'].map('{:,.2f}'.format)
 
-    # df['종가']=df['종가'].astype(float)
-    # df['종가']=df['종가'].apply(lambda x: round(x, 2))
-    df['PBR']=df['PBR'].apply(lambda x: round(x, 2))
-    # df['column_name'] = df['column_name'].apply(lambda x: round(x, 2))
 
 
     return df
