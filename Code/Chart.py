2757882ff7af3af9fbbe30ebb3649b850c3edd96
wndskim <wndskim01@gmail.com>
2023-01-07 16:54:39 +0900

    Update Chart.py

--- Code/Chart.py
+++ Code/Chart.py
@@ -16,6 +16,23 @@ def Chart_001(df):
     fig.update_layout(showlegend=False)
     fig.update_layout(barmode='group')
 
+    fig.update_layout(
+        xaxis_title="<b>날짜<b>",
+        yaxis_title="<b>PBR<b>",
+        width=1300,
+        height=500,
+        margin=dict(
+            l=50,
+            r=50,
+            b=50,
+            t=50,
+            pad=4
+            ),
+        plot_bgcolor='rgba(0,0,0,0)',
+        # yaxis=(dict(showgrid=False)),
+        paper_bgcolor='cornsilk'
+    )
+
     # Render the figure in the Streamlit app
     st.plotly_chart(fig)
 
