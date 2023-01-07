61830290ef46628e2f96757a5bc3e97f468d875d
wndskim <wndskim01@gmail.com>
2023-01-07 16:32:20 +0900

    Create Chart.py

---
+++ Code/Chart.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import plotly.express as px
+
+# Load data
+df = px.data.gapminder()
+
+def Chart_001(df):
+    # Create the Plotly figure
+    fig = px.bar(df, x="날짜", y="PBR", color="continent",
+                title="최근 20일 PBR",
+                template="plotly_dark")
+
+    # Render the figure in the Streamlit app
+    st.plotly_chart(fig)
+
+    return
