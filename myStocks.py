b9dc01e254940227eae31f84ea258acb334ded1a
wndskim <wndskim01@gmail.com>
2023-01-09 18:09:57 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -62,6 +62,11 @@ def main():
     # Side Bar 생성
     job=st.sidebar.selectbox('선택',['선택','년도별 가격 변동률 조회','관심주 및 보유주','특징주','매매기술 설명 보기',\
                              '가격 변동률(년간)','종목별 OHLCV','인덱스 종류'])
+
+    if job=='매매기술 설명 보기':
+        Strategy.Define_매매기술_설명()
+        return
+
     조회일=st.date_input('조회일')
 
     if job=='선택':
@@ -171,9 +176,6 @@ def main():
 
         df_종목=df[df['종목명']==종목명]
         st.dataframe(df_종목[['날짜','티커','종목명','사유_뉴스']])
-
-    if job=='매매기술 설명 보기':
-        Strategy.Define_매매기술_설명()
 
     if job=='가격 변동률(년간)':
         s선택=st.sidebar.selectbox('선택',['전체','코스피200','코스피','코스닥'])
