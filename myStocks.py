71d36bcfbb54888fe5d320eec360c1e09ce4dec0
wndskim <wndskim01@gmail.com>
2023-01-10 15:22:45 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -36,12 +36,11 @@ def 코스피200_등락률(sYear, sort_order):
     return df
 
 def 참조링크보기(티커):
-    종목=stock.get_market_ticker_name(티커)
-    st.write(f'[NICE CompanySearch](https://comp.kisline.com/hi/HI0100M010GE.nice?stockcd={종목}&nav=1)')
-    st.write(f'[CompanyGuide](https://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A{}&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701)'.format(종목))
-    st.write(f'[네이버금융(종합정보)](https://finance.naver.com/item/main.naver?code={})'.format(종목))
-    st.write(f'[ZOOM검색](https://search.zum.com/search.zum?method=uni&query={}&qm=f_instant.top)'.format(종목))
-    st.write(f'[다음통합검색](https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q={})'.format(종목))
+    st.write('[NICE CompanySearch](https://comp.kisline.com/hi/HI0100M010GE.nice?stockcd={}&nav=1)'.format(티커))
+    st.write('[CompanyGuide](https://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A{}&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701)'.format(티커))
+    st.write('[네이버금융(종합정보)](https://finance.naver.com/item/main.naver?code={})'.format(티커))
+    st.write('[ZOOM검색](https://search.zum.com/search.zum?method=uni&query={}&qm=f_instant.top)'.format(티커))
+    st.write('[다음통합검색](https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q={})'.format(티커))
     return
 
 def get_date(기준일, delta):
