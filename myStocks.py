aa3154fddcb28be533ebebb71fcdf48de520d528
wndskim <wndskim01@gmail.com>
2023-01-10 16:53:02 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -200,11 +200,14 @@ def main():
             종목=st.selectbox('발굴종목',종목명s)
             티커=df[df['종목']==종목]['티커'].values[0]
         with col2:
-            st.text('꼭 확인해야 할 사항 4가지')
-            st.text('1:부채비율, 2:유보율, 3:유통주식수, 4:적자흑자유무')
+            st.markdown('''
+                ###### :orange[꼭 확인해야 할 사항 4가지]
+                ###### :orange[1:부채비율, 2:유보율, 3:유통주식수, 4:적자흑자유무]
+                ''')
 
         col1, col2, col3=st.columns([1,2,2])
         with col1:
+            st.text('')
             참조링크보기(티커)
         with col2:
             재무정보=Dart.get_CompanyGuide자료(티커).transpose()
