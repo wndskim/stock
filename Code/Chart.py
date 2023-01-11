be0deb82c7ed97443c01e7037aaf37c5161a3886
wndskim <wndskim01@gmail.com>
2023-01-11 14:44:51 +0900

    Update Chart.py

--- Code/Chart.py
+++ Code/Chart.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import plotly.express as px
-
+import pandas as pd
+import plotly.graph_objects as go
 # Load data
 # df = px.data.gapminder()
+
+def Chart_002(data):
+    # Create the chart
+    fig = go.Figure(data=[go.Candlestick(x=data.날짜,
+                        open=data["시가"],
+                        high=data["고가"],
+                        low=data["저가"],
+                        close=data["종가"])])
+
+    # Add the chart to the app
+    st.plotly_chart(fig)
+
+    return
+
 
 def Chart_001(df):
     # Create the Plotly figure
