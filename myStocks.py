a206a1c0c81b11978450a785d50047d91f88963e
wndskim <wndskim01@gmail.com>
2023-01-11 17:27:50 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -12,7 +12,7 @@ import matplotlib as plt
 import yfinance as yf
 import datetime
 from datetime import date, timedelta
-from Code import Dart, Chart, Strategy
+from Code import Dart, Chart, Strategy, Display
 
 # @st.cache(suppress_st_warning=True)
 def 전종목_등락률(sYear, sort_order):
@@ -164,15 +164,17 @@ def main():
         st.write(시트선택, len(종목명s),'종목')
         st.dataframe(df)
 
-        col1, col2, col3=st.columns([1,2,1])
-        with col1:
-            종목=st.selectbox('발굴종목',종목명s)
-            티커=df[df['종목']==종목]['티커'].values[0]
-        with col2:
-            st.markdown('''
-                ###### :orange[꼭 확인해야 할 사항 4가지]
-                ###### :orange[1:부채비율, 2:유보율, 3:유통주식수, 4:적자흑자유무]
-                ''')
+        티커=Display.종목명_티커_선택(종목명s, df)
+        
+        # col1, col2, col3=st.columns([1,2,1])
+        # with col1:
+        #     종목=st.selectbox('발굴종목',종목명s)
+        #     티커=df[df['종목']==종목]['티커'].values[0]
+        # with col2:
+        #     st.markdown('''
+        #         ###### :orange[꼭 확인해야 할 사항 4가지]
+        #         ###### :orange[1:부채비율, 2:유보율, 3:유통주식수, 4:적자흑자유무]
+        #         ''')
 
         col1, col2, col3=st.columns([1,2,2])
         with col1:
