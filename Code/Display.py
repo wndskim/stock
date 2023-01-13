bbe3b7e1266d5464b288667782e6a24667edd14c
wndskim <wndskim01@gmail.com>
2023-01-13 12:28:03 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -16,15 +16,6 @@ def 참조링크보기(티커):
     return
 
 def 종목명_티커_선택(종목명s, df):
-    # col1, col2, col3=st.columns([1,2,1])
-    # with col1:
-    #     종목=st.selectbox('종목선택',종목명s)
-    #     티커=df[df['종목']==종목]['티커'].values[0]
-    # with col2:
-    #     st.markdown('''
-    #         ###### :orange[꼭 확인해야 할 사항 4가지]
-    #         ###### :orange[1:부채비율, 2:유보율, 3:유통주식수, 4:적자흑자유무]
-    #         ''')
 
     종목=st.sidebar.selectbox('종목선택',종목명s)
     티커=df[df['종목']==종목]['티커'].values[0]
