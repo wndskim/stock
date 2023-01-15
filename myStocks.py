90a298b0db1b38813bf68aae034b64a9c257f663
wndskim <wndskim01@gmail.com>
2023-01-15 16:49:32 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -74,10 +74,14 @@ def main():
             idx='1028'; freq='d'
             # df_kospi200=Dart.Index_OHLCV_조회(시작일, 종료일, idx, freq)
             # st.dataframe(df_kospi200)
-            인덱스s=stock.get_index_portfolio_deposit_file(idx)
-            for 티커 in 인덱스s:
-                st.write(티커,stock.get_market_ticker_name(티커))
-                # st.write(티커,stock.get_index_ticker_name(티커))
+
+            # 인덱스s=stock.get_index_portfolio_deposit_file(idx)
+            # for 티커 in 인덱스s:
+            #     st.write(티커,stock.get_market_ticker_name(티커))
+
+            data = yf.download("^KS200", start="2023-01-01", end="2023-01-15")
+            st.dataframe(data)
+
 
             return
 
