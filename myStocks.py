ba229392b803f3a9f8ffe0ab2ae2fabe0a77890e
wndskim <wndskim01@gmail.com>
2023-02-03 17:07:58 +0900

    Update myStocks.py

--- myStocks.py
+++ myStocks.py
@@ -134,7 +134,9 @@ def main():
 
             chk4=st.checkbox('2023년 투자전략 보기', value=False)
             if chk4:
-                Strategy.Strategy_2023_01(kospi_pbr)
+                선택=st.selectbox('선택',['2023년2월','2023년1월'])
+                if 선택=='2023년2월': Strategy.Strategy_2023('2월')
+                else: Strategy.Strategy_2023_01(kospi_pbr)
 
     if job=='년도별 가격 변동률 조회':
         년도=st.sidebar.selectbox('년도선택',('2023','2022','2021','2020','2019','2018'))
