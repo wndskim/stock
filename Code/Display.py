4d5d87f01dc1c2a858476ff6d1b4914992e0f952
wndskim <wndskim01@gmail.com>
2023-02-06 17:08:28 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -32,7 +32,7 @@ def 테마별_관심주보기():
 
         df3=df3[df3['설명']==테마선택]
         티커s=df3['티커'].tolist()
-        if radio2='120이평 작은순 보기':
+        if radio2=='120이평 작은순 보기':
             df2.values(by='이평120이격률',ascending=True,inplace=True)
         else: df2.values(by='이평120이격률',ascending=False,inplace=True)
         df2=df2[df2["티커"].isin(티커s)]
