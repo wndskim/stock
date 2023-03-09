6dd38da91747113c2edf703835ed3aa03cb1359d
wndskim <wndskim01@gmail.com>
2023-03-09 18:23:35 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -20,12 +20,12 @@ def 연방은행주요지표보기():
     return
 
 def 참조링크보기(종목):
-    st.write('[NICE CompanySearch](https://comp.kisline.com/hi/HI0100M010GE.nice?stockcd={}&nav=1)'.format(티커))
-    st.write('[CompanyGuide](https://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A{}&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701)'.format(티커))
-    st.write('[네이버금융(선도주확인용)](https://finance.naver.com/item/coinfo.naver?code={})'.format(티커))
-    st.write('[네이버(통합검색)](https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=1&acq=%EB%94%94%EC%BC%80%EC%9D%B4%EC%95%A4&qdt=0&ie=utf8&query={})'.format(티커))
-    st.write('[ZOOM검색](https://search.zum.com/search.zum?method=uni&query={}&qm=f_instant.top)'.format(티커))
-    st.write('[다음통합검색](https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q={})'.format(티커))
+    st.write('[NICE CompanySearch](https://comp.kisline.com/hi/HI0100M010GE.nice?stockcd={}&nav=1)'.format(종목))
+    st.write('[CompanyGuide](https://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A{}&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701)'.format(종목))
+    st.write('[네이버금융(선도주확인용)](https://finance.naver.com/item/coinfo.naver?code={})'.format(종목))
+    st.write('[네이버(통합검색)](https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=1&acq=%EB%94%94%EC%BC%80%EC%9D%B4%EC%95%A4&qdt=0&ie=utf8&query={})'.format(종목))
+    st.write('[ZOOM검색](https://search.zum.com/search.zum?method=uni&query={}&qm=f_instant.top)'.format(종목))
+    st.write('[다음통합검색](https://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q={})'.format(종목))
     return
 
 def 공용화면보기1(조회일,종목선택,티커,종목,df_종목,최고가,최저가,계산값1,계산값2,피보값):
