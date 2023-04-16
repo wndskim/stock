920d35a4a948ceda596e8229cd78e368f543e571
wndskim <wndskim01@gmail.com>
2023-04-16 09:31:00 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -214,7 +214,7 @@ def 거래량폭증_종목보기(조회일):
             else: pass
             # else: 위치=st.radio('단계선택',하락)
             
-            if 선택=='전체': df.sort_values(by=['위치','종목'], ascending=[True,False], inplace=True)
+            if 선택=='전체': df.sort_values(by=['위치','종목'], ascending=[True,True], inplace=True)
             else: df=df[df['위치']==위치]; df.sort_values(by='거래대금20대비', ascending=False, inplace=True)
 
             종목s=df['종목'].unique().tolist()
