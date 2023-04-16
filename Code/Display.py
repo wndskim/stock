3f973ee0b2cb3c7219e21ed8245d74a8a44555f2
wndskim <wndskim01@gmail.com>
2023-04-16 09:10:23 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -181,7 +181,7 @@ def 거래량폭증_종목보기(조회일):
         st.dataframe(df_종목)
 
     else:
-        st.caption('120일 상승종목은 2023년 3월 10일 부터 가능..!!')
+        st.caption('120일 상승종목은 2023년 3월 10일 부터 가능/RSI30이하,RSI70이상은 2023년4월14일부터 가능')
         df=pd.read_excel('./Data/거래량폭증종목.xlsx')
         df["티커"]=df["티커"].astype(int)
         df["티커"]=df["티커"].apply(lambda x: str(x).zfill(6))
