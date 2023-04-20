ecb93524227b8ea892c1397a7169a101df1606d9
wndskim <wndskim01@gmail.com>
2023-04-20 16:35:34 +0900

    Update Display.py

--- Code/Display.py
+++ Code/Display.py
@@ -35,7 +35,8 @@ def scrap_고객예탁금_신용잔고(page):
     return pd.read_html(url,encoding='utf-8')
 
 def 고객예탁금_신용잔고():
-    st.text('고객예탁금 대비 신용잔구 비율이 35%이상이면 과열신호로 관심을 가지고 본다(35%는 확인 필요)')
+    st.text('고객예탁금 대비 신용잔고 비율이 35%이상이면 과열신호로 관심을 가지고 본다(35%는 확인 필요)')
+    st.text('향후 차트를 추가 한다')
     df_concat=pd.DataFrame()
     for i in range(1,4):
         dfs=scrap_고객예탁금_신용잔고(i)[0]
