a7041047da76df1e6fb3c0776858e11ff8aa4545
wndskim <wndskim01@gmail.com>
2023-05-08 12:40:12 +0900

    Update GetData.py

--- Code/GetData.py
+++ Code/GetData.py
@@ -65,7 +65,7 @@ def 종목별_현재_재무정보(티커):
     재무상태표=tables[2]
     재무상태표=재무상태표.set_index(재무상태표.columns[0])
     자본총계=재무상태표.loc[['자본']]['2022/12'].values[0]
-    자본총계=자본총계.astype(int)
+    # 자본총계=자본총계.astype(int)
 
     # 시가총액
     url=f'https://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A{티커}&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701'
