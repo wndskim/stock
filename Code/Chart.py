484d584c12c5a80951cc0b67ba7159b7613a5ec6
wndskim <wndskim01@gmail.com>
2023-05-18 12:58:33 +0900

    Update Chart.py

--- Code/Chart.py
+++ Code/Chart.py
@@ -3,6 +3,38 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
+def 차트_시가총액(df,종목):
+    fig = go.Figure(data=[go.Candlestick(x=df['날짜'],
+                        open=df["시가"],
+                        high=df["고가"],
+                        low=df["저가"],
+                        close=df["종가"],
+                        name='일봉',
+                        increasing_line_color='red',
+                        increasing_fillcolor='red',
+                        decreasing_line_color='blue',
+                        decreasing_fillcolor='blue'
+                    ),
+                        go.Scatter(x=df['날짜'], y=df['sma5'], name='sma5'),
+                        go.Scatter(x=df['날짜'], y=df['sma10'], name='sma10'),
+                        go.Scatter(x=df['날짜'], y=df['sma20'], name='sma20'),
+                        go.Scatter(x=df['날짜'], y=df['sma60'], name='sma60'),
+                        go.Scatter(x=df['날짜'], y=df['sma120'], name='sma120'),
+                        go.Scatter(x=df['날짜'], y=df['sma240'], name='sma240'),
+                        go.Scatter(x=df['날짜'], y=df['sma480'], name='sma480')
+                    ])
+
+    fig.update_layout(title=종목+' 차트(일)',
+                    xaxis_title='날짜',
+                    yaxis_title='가격',
+                    width=1500,
+                    height=700,                     
+                    xaxis_rangeslider_visible = False)
+
+    st.plotly_chart(fig)
+
+    return
+
 def 차트_거래대금(df,종목,기간):
 
     fig = go.Figure()
