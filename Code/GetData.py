d102f7af1f41e7d35c827309642289488140b23e
wndskim <wndskim01@gmail.com>
2023-05-18 12:29:29 +0900

    Update GetData.py

--- Code/GetData.py
+++ Code/GetData.py
@@ -5,6 +5,10 @@ import requests
 import pandas as pd
 
 @st.cache_resource
+def 종목별_시가총액_기간(시작일,종료일,티커):
+    return stock.get_market_cap(시작일, 종료일, 티커)
+
+@st.cache_resource
 def load_from_pykrx_해당일전체(조회일):
     return stock.get_market_ohlcv(조회일)
 
