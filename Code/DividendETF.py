0019b3469dc45484c40854c1b3370560e4f87e68
wndskim <wndskim01@gmail.com>
2023-11-01 13:10:35 +0900

    Update DividendETF.py

--- Code/DividendETF.py
+++ Code/DividendETF.py
@@ -7,8 +7,9 @@ def get_info():
     ticker='TSLY'
     info=yf.Ticker(ticker)
 
-    history=pd.DataFrame(info.history())
-    history.Date=history.Date.strftime('%Y-%m-%d')
+    history=info.history()
+    history=pd.DataFrame(history)
+    # history.Date=history.Date.strftime('%Y-%m-%d')
 
     st.write(history)
 
