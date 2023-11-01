471d0388e21bfb32310bea2b0cef8d7f634bd822
wndskim <wndskim01@gmail.com>
2023-11-01 13:25:07 +0900

    Update DividendETF.py

--- Code/DividendETF.py
+++ Code/DividendETF.py
@@ -9,7 +9,7 @@ def get_info():
 
     history=info.history(interval='1d',start='2013-01-01',end='2023-10-31')
     history=pd.DataFrame(history)
-    history.set_index(inplace=True)
+    history.set_index(inplace=True,keys='Date')
     history.index=history.index.strftime('%Y-%m-%d')
 
     # st.write(type(history))
